67d0f18a195a08bf78d2374591650dbf01c742b0
yair123123 <ytykhn8@gmail.com>
2024-08-11 09:44:33 +0300

    עדכון של CountTil

--- DefenseSimulator/Controllers/CountTilsController.cs
+++ DefenseSimulator/Controllers/CountTilsController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DefenseSimulator.Data;
 using DefenseSimulator.Models;
+using Azure;
+using System.Data;
 
 namespace DefenseSimulator.Controllers
 {
@@ -28,13 +30,46 @@ namespace DefenseSimulator.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,ElectronicJamming,AntiAirSystem,InterceptorMissile")] CountTil countTil)
         {
+
             if (ModelState.IsValid)
             {
-                _context.Add(countTil);
-                await _context.SaveChangesAsync();
-                return RedirectToAction( "Index", "Threats");
+                try
+                {
+                    if (!_context.CountTil.Any())
+                    {
+                        _context.Add(countTil);
+                        await _context.SaveChangesAsync();
+                        return RedirectToAction("Index", "Threats");
+                    }
+                    var countTil1 = await _context.CountTil.FirstOrDefaultAsync();
+                    countTil1.InterceptorMissile = countTil.InterceptorMissile;
+                    countTil1.ElectronicJamming = countTil.ElectronicJamming;
+                    countTil1.AntiAirSystem = countTil.AntiAirSystem;
+                    _context.Update(countTil1);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CountTilExists(countTil.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index", "Threats");
+
+                //_context.Add(countTil);
+                //await _context.SaveChangesAsync();
+                //return RedirectToAction( "Index", "Threats");
             }
             return View(countTil);
+        }
+        private bool CountTilExists(int id)
+        {
+            return _context.CountTil.Any(e => e.Id == id);
         }
     }
 }
